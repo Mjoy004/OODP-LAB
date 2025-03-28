b42ab0066062b91ad516f6e91ff3f165db0071ad
Mrityunjoy Karan <mjoykaran004@gmail.com>
2025-03-29 00:15:04 +0530

    Create TYPE CASTING .java

---
+++ TYPE CASTING .java
@@ -0,0 +1,8 @@
+public class WideningCasting {
+    public static void main(String[] args) {
+        int num = 10;
+        double d = num;  // int to double (automatic)
+        System.out.println("Integer: " + num);
+        System.out.println("Double: " + d);
+    }
+}
